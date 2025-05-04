713034792a1e1ee243e0bc8f1dfefeea12813efc
Daniel Laera <laera.daniel@hotmail.com>
2025-05-04 12:28:10 +0200

    first commit

---
+++ src/main/java/com/daniellaera/caffeine_backend/CaffeineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daniellaera.caffeine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffeineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniellaera/caffeine_backend/CaffeineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniellaera.caffeine_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
