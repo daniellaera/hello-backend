42fd11add0bc58535fd6d3f650d236f458147a0c
Daniel Laera <laera.daniel@hotmail.com>
2025-05-27 22:42:40 +0200

    rollback test 2

--- src/main/java/com/daniellaera/caffeine_backend/controller/HelloController.java
+++ src/main/java/com/daniellaera/caffeine_backend/controller/HelloController.java
@@ -21,6 +21,11 @@ public class HelloController {
         return "Test page!";
     }
 
+    @GetMapping("/test2")
+    public String test2() {
+        return "Test page 2!";
+    }
+
     @GetMapping("/world")
     public String world() {
         return "Test world endpoint!";
